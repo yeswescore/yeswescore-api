edf79e99ba7e86f7959d1bb0591bd908879de7f4
Marc Dassonneville <marc.dassonneville@gmail.com>
2013-01-09 22:48:03 +0100

    UT: clubs first test passed.

--- ut/server/test/dev/api.clubs.js
+++ ut/server/test/dev/api.clubs.js
@@ -6,6 +6,7 @@ if (Conf.env !== "DEV")
   process.exit(0);
 
 describe('dev:clubs', function(){
+  // READ
   describe('read random document club, then read api club should be a valid club', function(){
     it('should give club (not empty & valid)', function (done){
       var options = {
@@ -16,30 +17,37 @@ describe('dev:clubs', function(){
       
       http.getJSON(options, function (randomclub) {
         assert.isObject(randomclub, "random club must exist");
-        
+
         var options = {
           host: Conf["http.host"],
           port: Conf["http.port"],
-          path: Conf["api.clubs"]+randomclub.id
+          path: Conf["api.clubs"]+randomclub._id
         };
         http.getJSON(options, function (club) {
           assert.isClub(club, "must be a club");
-          assert(club.id === randomclub.id, "must be same club");
+          assert(club.id === randomclub._id, "must be same club");
           done();
         });
       });
     });
   });
+  
+  // PARAMETERS
+  describe('FIXME: read api club using wrong id parameters', function() {
+    it('should throw an error', function (done) {
+      done(/* FIXME */);
+    });
+  });
 
-  describe('read random document club, then read api club filtering fields', function() {
+  describe('FIXME: read random document club, then read api club filtering fields', function() {
     it('should filter fields using fields= option', function (done) {
-      done("FIXME");
+      done(/* FIXME */);
     });
   });
   
   describe('FIXME: club creation', function() {
     it('should create clubs', function (done) {
-      done("FIXME");
+      done(/* FIXME */);
     });
   });
 });
