6b415320079c3a1a977ef280a391c192f3ca77d1
Marc Dassonneville <marc.dassonneville@gmail.com>
2013-01-23 20:52:57 +0100

    dev: adding .port file feature

--- server/dev.sh
+++ server/dev.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
 export NODE_ENV="DEV"
-if [ "$1" = "debug" ]
+if [ -f ".port" ]
 then
-  node --debug server.js
+  port=`cat .port | head -1`
+  echo "using port number $port from file .port"
+  echo "mongo db will be dev$port"
+  export ZESCORE_PORT=$port
+  if [ "$1" = "debug" ]
+  then
+    echo "debug mode activated"
+    node --debug server.js
+  else
+    node server.js
+  fi
 else
-  node server.js
+  echo "  Please create .port file containing port number "
+  echo "  Exemple:  echo \"15123\" > .port "
 fi
