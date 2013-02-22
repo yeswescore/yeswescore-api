a66ee4c6847cc987e32009ac36d997604a4bf3f1
Marc Dassonneville <marc.dassonneville@gmail.com>
2013-02-22 12:00:54 +0100

    removing client from server app

--- client/gen/com/zenodus/client/html5/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.zenodus.client.html5;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- client/zipSource.bat
+++
@@ -1,2 +0,0 @@
-del C:\Users\Docteur-SAV\Desktop\projet.zip
-7z a C:\Users\Docteur-SAV\Desktop\projet.zip assets/www/*
