bccdbd34778bc0755359c16bcffb312a42216e3f
Marc Dassonneville <marc.dassonneville@gmail.com>
2013-03-14 17:19:23 +0100

    api/connections => api/facebook

--- server/api/facebook.js
+++ server/api/facebook.js
@@ -2,6 +2,7 @@ var DB = require("../db.js")
   , express = require("express")
   , app = require("../app.js")
   , Conf = require("../conf.js")
+  , https = require("../https.js")
   , Q = require("q");
 
 /**
@@ -10,12 +11,11 @@ var DB = require("../db.js")
  * You must be authentified (?playerid=...&token=...)
  * 
  * 4 Parameters are required :
- * /v1/connections/fb/?playerid=...   player id
- * /v1/connections/fb/?token=...      player token
- * /v1/connections/fb/?fbid=...       facebook id
- * /v1/connections/fb/?fbtoken=...    facebook token
+ * /v1/connections/fb/?playerid=...       player id
+ * /v1/connections/fb/?token=...          player token
+ * /v1/connections/fb/?access_token=...   facebook access_token
  */
-app.get('/v1/connections/fb/', function(req, res){
+app.get('/v1/facebook/login/', function(req, res){
   var scheme = Conf.get("facebook.yws.scheme")
     , host = Conf.get("facebook.yws.host")
     , port = Conf.get("facebook.yws.port");
@@ -26,6 +26,23 @@ app.get('/v1/connections/fb/', function(req, res){
         throw "player not authenticated";
       return authentifiedPlayer;
     }).then(function (player) {
+      // checking fb token
+      
+      
+      // connecting to facebook...
+      var clientId = Conf.get('facebook.app.id')
+        , clientSecret = Conf.get('facebook.app.secret');
+      
+      https.get({
+        host: Conf.get('facebook.graph.host'),
+        port: Conf.get('facebook.graph.port'),
+        path: "oauth/access_token?" +
+           "grant_type=fb_exchange_token&" +
+           "client_id=" + clientId + "&" +
+           "client_secret=" + clientSecret + "&"
+           "fb_exchange_token
+      });
+      
       // saving facebook id & token.
       player.connection.facebook.id = req.query.fbid;
       player.connection.facebook.token = req.query.fbtoken;
