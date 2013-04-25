944d0adc10d16844cc7743987b5ace55ded06c1d
Vincent Terrasi <vincent@yeswescore.com>
2013-04-26 00:45:57 +0300

    Update bootstrap.js
    test app.deprecated

--- server/api/bootstrap.js
+++ server/api/bootstrap.js
@@ -16,7 +16,7 @@ app.get('/bootstrap/conf.json', function(req, res){
       conf = [
         { key: 'version.latest', value: latest, metadata: {} },
         { key: 'bootstrap.update_interval', value: 24 * 3600 * 1000, metadata: {} }, // every day
-        { key: 'api.clubs', value: baseUrl+Conf.get("api.clubs"), metadata: {} }
+        { key: 'app.deprecated', value: true, metadata: {} }
       ];
       break;
   }
