f64fa29e95bf43eb5cb3fcaec8dfcaea761e9e47
Vincent Terrasi <vincent@yeswescore.com>
2013-04-26 00:36:35 +0300

    Update bootstrap.js
    simplify conf variables

--- server/api/bootstrap.js
+++ server/api/bootstrap.js
@@ -16,8 +16,6 @@ app.get('/bootstrap/conf.json', function(req, res){
       conf = [
         { key: 'version.latest', value: latest, metadata: {} },
         { key: 'bootstrap.update_interval', value: 24 * 3600 * 1000, metadata: {} }, // every day
-        { key: 'api.games', value: baseUrl+Conf.get("api.games"), metadata: {} },
-        { key: 'api.players', value: baseUrl+Conf.get("api.players"), metadata: {} },
         { key: 'api.clubs', value: baseUrl+Conf.get("api.clubs"), metadata: {} }
       ];
       break;
