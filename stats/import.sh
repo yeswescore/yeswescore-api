db9a9bb29d9d51dbadc56f69c1469c4cdd93c47b
Marc Dassonneville <marc.dassonneville@gmail.com>
2013-09-25 22:31:45 +0200

    stats: nouvelles url

--- stats/import.sh
+++ stats/import.sh
@@ -11,13 +11,18 @@ cd ~/tmp/stats
 rm -rf ~/tmp/stats/dump/
 tar -xvjf $latest
 # restoring collections
+mongo stats --eval 'db.clubs.remove()'
+mongo stats --eval 'db.games.remove()'
+mongo stats --eval 'db.files.remove()'
+mongo stats --eval 'db.players.remove()'
 mongorestore --collection clubs --db stats dump/prod/clubs.bson
 mongorestore --collection games --db stats dump/prod/games.bson
 mongorestore --collection files --db stats dump/prod/files.bson
 mongorestore --collection players --db stats dump/prod/players.bson
 # IMPORTING PROD STATS
-sudo rsync -rltov -e 'ssh -p 42' root@188.165.247.143:/var/log/yeswescore-server/stats.log ~/tmp/stats/
+#sudo rsync -rltov -e 'ssh -p 42' root@188.165.247.143:/var/log/yeswescore-server/stats.log ~/tmp/stats/
+cp /var/log/yeswescore-server/stats.log ~/tmp/stats/
 cat stats.log | cut -c 7- > stats.csv
 # processing data with node
-cd $BASEDIR;
-node process.js
+#cd $BASEDIR;
+#node process.js
